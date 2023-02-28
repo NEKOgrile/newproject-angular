60589d04a7c85c3294579531de6ccef3dc028c5a
NEKOgrile <willemcornil@gmail.com>
2023-02-28 16:56:24 +0100

    fini

--- tips/src/app/inserer/inserer.component.ts
+++ tips/src/app/inserer/inserer.component.ts
@@ -16,23 +16,34 @@ export class InsererComponent{
 @Input() nbrpersonne : number = 0;
 @Input() prix: number = 0;
 
-general : number = 0;
+tipspersonnecalcul : number = 0;
+prixpersonnecalcul : number = 0;
 
 ngOnChanges() {
   this.tipspersonne(this.prix , this.tips , this.nbrpersonne);
+  this.prixpersonne(this.prix , this.tips , this.nbrpersonne);
 }
 
 
 
-sendtofather()
-  {
-    this.SendRequestToFather.emit(this.prix);
-    this.SendRequestToFather2.emit(this.tips);
-    //this.SendRequestToFather.emit(this.nbrpersonne);
-  }
+
 
   tipspersonne(prix: any , tips : any , nbrpersonne : any) {
-    this.nbrpersonne = parseInt(prix, 10) + parseInt(tips, 10) +  parseInt(nbrpersonne, 10);
+    this.tipspersonnecalcul = parseInt(prix, 10)  * (0.01 * parseInt(tips, 10))/ parseInt(nbrpersonne, 10);
   }
+  
+  prixpersonne(prix: any , tips : any , nbrpersonne : any) {
+    this.prixpersonnecalcul = parseInt(prix, 10)  * (1+(0.01 * parseInt(tips, 10)))/ parseInt(nbrpersonne, 10);
+  }
+
+
+
+  sendtofather()
+  {
+    this.SendRequestToFather.emit(this.tipspersonnecalcul);
+    this.SendRequestToFather2.emit(this.prixpersonnecalcul);
+
+  }
+
 }
 
